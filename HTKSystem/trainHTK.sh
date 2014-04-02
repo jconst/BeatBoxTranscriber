32210b692eac9fa635f4516a07d5bfbd3844fa98
Joseph Constantakis <jcon5294@gmail.com>
2014-04-02 13:28:30 -0400

    switching from HCompV to HInit and HRest

--- HTKSystem/trainHTK.sh
+++ HTKSystem/trainHTK.sh
@@ -28,40 +28,43 @@ HCopy -T 1 -C needed/config -S codetr.scp
 
 # create proto
 perl needed/createproto.pl
-HCompV -C needed/config -f 0.01 -m -S train.scp -M hmm0 proto
+HInit -C needed/config -S train.scp -M hmm0/ proto
 perl needed/createhmmdefs.pl hmm0
 
-# do for hmm1, hmm2, hmm3
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm0/macros -H hmm0/hmmdefs -M hmm1/ monophones0
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm1/macros -H hmm1/hmmdefs -M hmm2/ monophones0
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm2/macros -H hmm2/hmmdefs -M hmm3/ monophones0
+# HRest -C needed/config -I phones0.mlf -S train.scp -H hmm0/macros -H hmm0/hmmdefs -M hmm1/
 
-# create an sp model
-perl needed/makeSPmodel.pl hmm3
+# # do for hmm1, hmm2, hmm3
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm1/macros -H hmm1/hmmdefs -M hmm2/ monophones0
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm2/macros -H hmm2/hmmdefs -M hmm3/ monophones0
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm3/macros -H hmm3/hmmdefs -M hmm4/ monophones0
 
-# tie sp to center sil state
-HHEd -H hmm4/macros -H hmm4/hmmdefs -M hmm5 sil.hed monophones1
+# # create an sp model
+# perl needed/makeSPmodel.pl hmm4
 
-# two more passes of HERest
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm5/macros -H hmm5/hmmdefs -M hmm6/ monophones1
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm6/macros -H hmm6/hmmdefs -M hmm7/ monophones1
+# # tie sp to center sil state
+# HHEd -H hmm4/macros -H hmm5/hmmdefs -M hmm6 sil.hed monophones1
 
-# add silence to the dictionary
-perl needed/addsilence.pl
+# # two more passes of HERest
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm6/macros -H hmm6/hmmdefs -M hmm7/ monophones1
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm7/macros -H hmm7/hmmdefs -M hmm8/ monophones1
 
-# create label files
-perl needed/createlab.pl
+# # add silence to the dictionary
+# perl needed/addsilence.pl
 
-# run Viterbi decoding
-HVite -l '*' -o SWT -b silence -C needed/config -a -H hmm7/macros -H hmm7/hmmdefs -i aligned.mlf -m -t 250.0 150.0 1000.0 -y lab -I words.mlf -S train.scp dict monophones1
+# # create label files
+# perl needed/createlab.pl
 
-# two more passes of HERest
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm7/macros -H hmm7/hmmdefs -M hmm8/ monophones1
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm8/macros -H hmm8/hmmdefs -M hmm9/ monophones1
+# # run Viterbi decoding
+# HVite -l '*' -o SWT -b silence -C needed/config -a -H hmm8/macros -H hmm8/hmmdefs -i aligned.mlf -m -t 250.0 150.0 1000.0 -y lab -I words.mlf -S train.scp dict monophones1
 
-HHEd -H hmm9/macros -H hmm9/hmmdefs -M hmm10/ needed/mix.hed monophones0
+# # two more passes of HERest
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm8/macros -H hmm8/hmmdefs -M hmm9/ monophones1
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm9/macros -H hmm9/hmmdefs -M hmm10/ monophones1
 
-# two more passes of HERest
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm10/macros -H hmm10/hmmdefs -M hmm11/ monophones1
-HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm11/macros -H hmm11/hmmdefs -M hmm12/ monophones1
+# # increase number of Gaussians in the Mixture model
+# HHEd -H hmm10/macros -H hmm10/hmmdefs -M hmm11/ needed/mix.hed monophones0
+
+# # two more passes of HERest
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm11/macros -H hmm11/hmmdefs -M hmm12/ monophones1
+# HERest -C needed/config -I phones0.mlf -t 250.0 150.0 1000.0 -S train.scp -H hmm12/macros -H hmm12/hmmdefs -M hmm13/ monophones1
 
